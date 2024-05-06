0d27e65db36ccbce5036ecc814d254bdfda52d03
Ranjan Manandhar <ranjan@ranjan.local>
2024-05-06 14:39:57 +0545

    Singly Linked list

--- LinkedList/MyLinkedList.py
+++ LinkedList/MyLinkedList.py
@@ -1,13 +1,16 @@
-class MyLinkedList:
-
-    def __init__(self, val=0, next=None):
+class Node:
+    def __init__(self, val):
         self.val = val
-        self.next = next
+        self.next = None
+
+class MyLinkedList:
+    def __init__(self):
+        self.length = 0
+        self.dummy = Node(0)
 
     def get(self, index: int) -> int:
         count = 0
-        current = self
-        print(current)
+        current = self.dummy
         while current:
             if count == index:
                 return current.val
@@ -16,59 +19,67 @@ class MyLinkedList:
         return -1
 
     def addAtHead(self, val: int) -> None:
-        new_node = MyLinkedList(val)
-        new_node.next = self
-        return new_node
+       if self.length < 1:
+            self.dummy = Node(val)
+            self.length += 1
+            return
+       current = self.dummy
+       self.dummy = Node(val)
+       self.dummy.next = current
+       self.length += 1
 
     def addAtTail(self, val: int) -> None:
-        new_node = MyLinkedList(val)
-        if not self:
-            self = new_node
+        if self.length < 1:
+            self.dummy = Node(val)
+            self.length += 1
             return
-        current = self
+        new_node = Node(val)
+        current = self.dummy
         while current.next:
             current = current.next
         current.next = new_node
+        self.length += 1
+        
 
     def addAtIndex(self, index: int, val: int) -> None:
         if index == 0:
             return self.addAtHead(val)
-        new_node = MyLinkedList(val)
+        new_node = Node(val)
         count = 0
-        current = self
+        current = self.dummy
         while current:
             if count == index - 1:
                 new_node.next = current.next
                 current.next = new_node
+                self.length += 1
                 return
             current = current.next
             count += 1
         self.addAtTail(val)
 
     def deleteAtIndex(self, index: int) -> None:
+        if index < 0:
+            return
         if index == 0:
-            if self:
-                return self.next
-            else:
-                return None
-        current = self
-        prev = None
-        count = 0
+            if self.dummy.next:
+                self.dummy.next = self.dummy.next.next
+                self.length -= 1
+            return
+        current = self.dummy.next
+        prev = self.dummy  # Set prev to dummy node initially
+        count = 0        
         while current:
             if count == index:
-                if prev:
-                    prev.next = current.next
+                prev.next = current.next
+                self.length -= 1  
                 return
             prev = current
             current = current.next
             count += 1
 
-    def printList(self):
-        current = self
-        while current:
-            print(current.val, end=" -> ")
-            current = current.next
-        print("None")
+
+
+
 # Your MyLinkedList object will be instantiated and called as such:
 # obj = MyLinkedList()
 # param_1 = obj.get(index)
