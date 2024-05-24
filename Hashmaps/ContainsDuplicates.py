a28b2535eadab5564f193832e8e03ed901882193
Ranjan Manandhar <ranjan@ranjan.local>
2024-05-24 19:19:32 +0545

    nearby duplicates

--- Hashmaps/ContainsDuplicates.py
+++ Hashmaps/ContainsDuplicates.py
@@ -10,15 +10,18 @@ class Solution:
         return False
     
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        counter = 1
-        
-        for num in nums:
-            if num == k:
-                counter += 1
-        
-        if counter > 1 and counter % 2 == 0:
-            return True
-        
+        seen = {}  # Dictionary to store the most recent index of each element
+
+        for i, num in enumerate(nums):
+            # If the current number is already in the dictionary and the difference
+            # between the current index and the previous index of the number is less than or equal to k,
+            # then we have found nearby duplicates
+            if num in seen and i - seen[num] <= k:
+                return True
+            # Update the index of the current number in the dictionary
+            seen[num] = i
+
+        # If no nearby duplicates were found
         return False
        
     
